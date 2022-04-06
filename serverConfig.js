155f2c025b753ca2efc5e1b277a32d9cf935b5dd
BANGBANGROX <lakshya0809bang@gmail.com>
2022-04-06 14:59:41 +0530

    Added config file

---
+++ serverConfig.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://LakshyaBang:lakshya@nodeexpressproject.tsl1f.mongodb.net/SOCIAL-MEDIA-APP?retryWrites=true&w=majority",
+  SECRET_KEY: "some very secret key",
+};
